e3e303bb75872875b97a51952756260cdca63215
Govind Upadhyay <122630411+govindop07@users.noreply.github.com>
2025-10-07 21:37:12 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (87.26%) - LeetSync

--- 500-keyboard-row/keyboard-row.java
+++ 500-keyboard-row/keyboard-row.java
@@ -6,15 +6,15 @@ class Solution {
         String third = "zxcvbnm";
         
         for(String word: words) {
-            if(check(word, first) || check(word, second) || check(word, third)) list.add(word);
+            String lower = word.toLowerCase();
+            if(check(lower, first) || check(lower, second) || check(lower, third)) list.add(word);
         }
         return list.toArray(new String[0]);
     }
 
     public boolean check(String s, String ragex) {
-        String capitalRagex = ragex.toUpperCase();
         for(char c: s.toCharArray()) {
-            if(ragex.indexOf(c) == -1 && capitalRagex.indexOf(c) == -1) return false;
+            if(ragex.indexOf(c) == -1) return false;
         }
         
         return true;

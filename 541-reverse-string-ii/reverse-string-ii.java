c3cb04506207f14ee7d539b655d0b678f2908187
Govind Upadhyay <122630411+govindop07@users.noreply.github.com>
2025-10-07 19:45:33 +0530

    Time: 1 ms (81.79%) | Memory: 44.3 MB (12.03%) - LeetSync

---
+++ 541-reverse-string-ii/reverse-string-ii.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String reverseStr(String s, int k) {
+        if(s.length() <= 1) return s;
+        if(s.length() == k) return new StringBuilder(s).reverse().toString();
+
+        int idx = 0, n = s.length();
+        boolean flag = true;
+        StringBuilder ans = new StringBuilder();
+
+        while(idx <= n-k) {
+            if(flag) {
+                StringBuilder str = new StringBuilder(s.substring(idx, idx+k));
+                String reversed = str.reverse().toString();
+                ans.append(reversed);
+                idx += k;
+                flag = false;
+            } else {
+                ans.append(s.substring(idx, idx+k));
+                idx += k;
+                flag = true;
+            }
+        }
+
+        StringBuilder str = new StringBuilder(s.substring(idx));
+        if(flag) {
+            String reversed = str.reverse().toString();
+            ans.append(reversed);
+        }
+        else ans.append(str);
+
+        return ans.toString();
+    }
+}
